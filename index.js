b595507c261f350919cedbb87ca90ed9d1bc5188
Breno de Barros <brenowbarros21@gmail.com>
2022-02-08 11:33:43 -0400

    Capturando dados do formulario

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Definindo o EJS como engine do HTML
 app.set('view engine', 'ejs');
 //Configurando arquivos estáticos
 app.use(express.static('public'));
 
+//Configurando body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
 app.get('/', (req ,res) => {
     res.render('index');
 });
@@ -14,9 +20,16 @@ app.get('/perguntar', (req, res) => {
     res.render('perguntar');
 });
 
+app.post('/salvarpergunta', (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário enviado com sucesso!");
+});
+
+//Inicia o servidor
 app.listen(8080, (error) => {
     if (error) {
         console.log("Ocorreu um erro: " + error);
     }
     console.log("Servidor iniciado!");
-})
+});
